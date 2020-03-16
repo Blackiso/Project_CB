2f11f115811c4d382e47af89c8af51b1120baa38
Blackiso <ismailboy00@gmail.com>
2020-03-17 00:55:32 +0100

    Updated test files

--- WS-test/script.js
+++ WS-test/script.js
@@ -12,13 +12,16 @@ var connect = document.querySelector('#connect');
 var disconnect = document.querySelector('#disconnect');
 var send = document.querySelector('#send');
 var msg = document.querySelector('#msg');
-var host = document.querySelector('#host');
+var token = document.querySelector('#token');
+var room = document.querySelector('#room');
+var join = document.querySelector('#join');
+
+token.value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE1ODI3NDA2MDc5NDIsInVubSI6IkJsYWNraXNvIiwiZW1sIjoiYmxhY2tAZW1haWwuY29tIiwiaWF0IjoxNTg0MjkzNDc5LCJleHAiOjE1ODQ4OTgyNzl9.4EluMk09gy5tZBO60LTOZEFN-g9hYPWRPYCOCcEKXnA";
 
 connect.addEventListener('click', () => {
 
-	var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE1ODI3NDA2MDc5NDIsInVubSI6IkJsYWNraXNvIiwiZW1sIjoiYmxhY2tAZW1haWwuY29tIiwiaWF0IjoxNTg0MjkzNDc5LCJleHAiOjE1ODQ4OTgyNzl9.4EluMk09gy5tZBO60LTOZEFN-g9hYPWRPYCOCcEKXnA";
-	var hv = host.value == '' ? 'http://127.0.0.1' : host.value;
-	mySocket = io.connect(hv, { query: {token: token} });
+	var t = token.value;
+	mySocket = io.connect('http://127.0.0.1', { query: {token: t} });
 
 	mySocket.on('connect', () => {
 		insertLog('INFO', 'Socket Connected!');
